a587cb777b68bfd9256a3399b10a63adb25b9bac
Stephanie Fashagba <stephanie.fashagba@thoughtworks.com>
2020-05-03 14:04:01 +0100

    Adding local server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './ConvexGeometry';
+import App from './TrippyTripTrip';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
